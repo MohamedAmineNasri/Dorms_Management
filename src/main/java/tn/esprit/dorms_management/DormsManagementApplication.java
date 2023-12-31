80f9a36d3b4e5804db00dc2f01e5f61d90a7f7a5
MohamedAmineNasri <mohamedamine.nasri@esprit.tn>
2023-12-31 13:15:05 +0100

    First Commit

---
+++ src/main/java/tn/esprit/dorms_management/DormsManagementApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.dorms_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DormsManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DormsManagementApplication.class, args);
+    }
+
+}
